638e3f9d86c7f94bc88b7279777141f9ce230440
Andrew Avola <76986167+Meatballin@users.noreply.github.com>
2022-03-14 21:11:42 -0700

    Added creation of sphere objects

--- Assignment4/main.js
+++ Assignment4/main.js
@@ -1,7 +1,11 @@
 
-"use strict";
-
 var gl;
+var near;
+var far;
+var d;
+var year = 0;
+var day = 0;
+
 
 function init() {
     var canvas = document.getElementById("webgl-canvas");
@@ -11,7 +15,33 @@ function init() {
     gl.enable(gl.DEPTH_TEST);
    
     // Add your sphere creation and configuration code here
+    //===================
+    //SUN
+    //===================
+    Sun = new Sphere();
+    Sun.color = vec4(1, 1, 0, 1);
+    Sun.radius = 15;
 
+    //===================
+    //EARTH
+    //===================
+    Earth = new Sphere();
+    Earth.color = vec4(0, 0, 1, 1);
+    Earth.radius = 3;
+    Earth.orbit = 30;
+
+    //===================
+    //MOON
+    //===================
+    Moon = new Sphere();
+    Moon.color = vec4(1, 1, 1, 1);
+    Moon.radius = 1;
+    Moon.orbit = 8;
+
+    near = 1;
+    d = 2 * (Earth.orbit + Moon.orbit + Moon.radius + 5000);
+    far = near + d;
+    
     requestAnimationFrame(render);
 }
 
