2e6da4e7a194fb9db1865c17c6e64bdb5d0955b8
Andrew Avola <76986167+Meatballin@users.noreply.github.com>
2022-05-13 01:56:50 -0700

    Added Lighting
    Self teaching THREE.js and added multiple point lights to the scene with sliders  to determine position and intensity

--- finalprojecttest/src/script.js
+++ finalprojecttest/src/script.js
@@ -3,6 +3,11 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'dat.gui'
 
+//Texture reference
+const textureLoader = new THREE.TextureLoader();
+
+const normalTexture = textureLoader.load('/textures/test.png');
+
 // Debug
 const gui = new dat.GUI()
 
@@ -13,12 +18,18 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 // Objects
-const geometry = new THREE.TorusGeometry( .7, .2, 16, 100 );
+const geometry = new THREE.TorusGeometry( .5, .1, 20, 100);
 
 // Materials
 
-const material = new THREE.MeshBasicMaterial()
-material.color = new THREE.Color(0xff0000)
+const material = new THREE.MeshStandardMaterial()
+//Lavender color
+material.color = new THREE.Color(0x292929)
+material.roughness = .2
+material.metalness = .7
+
+material.normalMap = normalTexture;
+
 
 // Mesh
 const sphere = new THREE.Mesh(geometry,material)
@@ -26,12 +37,31 @@ scene.add(sphere)
 
 // Lights
 
-const pointLight = new THREE.PointLight(0xffffff, 0.1)
-pointLight.position.x = 2
-pointLight.position.y = 3
-pointLight.position.z = 4
+const pointLight = new THREE.PointLight(0xffffff, 2)
+pointLight.position.x = 0
+pointLight.position.y = 15
+pointLight.position.z = 0
 scene.add(pointLight)
 
+const light1 = gui.addFolder('Light 1')
+light1.add(pointLight.position, 'y').min(-3).max(3).step(0.01)
+light1.add(pointLight.position, 'x').min(-6).max(6).step(0.01)
+light1.add(pointLight.position, 'z').min(-3).max(3).step(0.01)
+light1.add(pointLight, 'intensity').min(0).max(10).step(0.01)
+
+const pointLight2 = new THREE.PointLight(0xffffff, 2)
+pointLight2.position.set(1.08,.09,-1.32)
+pointLight2.intensity = 5
+scene.add(pointLight2)
+
+const light2 = gui.addFolder('Light 2')
+light2.add(pointLight2.position, 'y').min(-3).max(3).step(0.01)
+light2.add(pointLight2.position, 'x').min(-6).max(6).step(0.01)
+light2.add(pointLight2.position, 'z').min(-3).max(3).step(0.01)
+light2.add(pointLight2, 'intensity').min(0).max(10).step(0.01)
+
+const pointLightHelper = new THREE.PointLightHelper(pointLight2, 1)
+scene.add(pointLightHelper)
 /**
  * Sizes
  */
@@ -73,7 +103,9 @@ scene.add(camera)
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+    canvas: canvas,
+    alpha: true
+    
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
@@ -82,15 +114,21 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
  * Animate
  */
 
+
+
+
 const clock = new THREE.Clock()
 
 const tick = () =>
 {
+    
 
     const elapsedTime = clock.getElapsedTime()
-
+    
     // Update objects
-    sphere.rotation.y = .5 * elapsedTime
+    sphere.rotation.y = 1 * elapsedTime
+
+    
 
     // Update Orbital Controls
     // controls.update()
